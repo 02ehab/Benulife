d4e99c418277cb2c41836e10284396d1602e9374
Ehab <40764172+02ehab@users.noreply.github.com>
2025-07-14 14:56:06 +0300

    Add files via upload

---
+++ track_order.js
@@ -0,0 +1,35 @@
+window.openMenu = function () {
+  document.getElementById("sideMenu").classList.add("open");
+}
+
+window.closeMenu = function () {
+  document.getElementById("sideMenu").classList.remove("open");
+}
+
+ // تغيير صفحة الطلبات علي نوع المستخدم
+document.addEventListener("DOMContentLoaded", () => {
+  const userType = localStorage.getItem("userType"); // "donor", "hospital", "bloodbank"
+
+  const linkText = (userType === "hospital" || userType === "bloodbank")
+    ? "طلبات المتبرعين"
+    : "الطلبات العاجلة";
+
+  const linkHref = (userType === "hospital" || userType === "bloodbank")
+    ? "donate_card.html"
+    : "emergency_card.html";
+
+  const linkHTML = `<a href="${linkHref}">${linkText}</a>`;
+
+  // تعويض أماكن الروابط
+  const placeholders = [
+    document.getElementById("requestsLinkPlaceholder"),
+    document.getElementById("requestsLinkDesktop"),
+    document.getElementById("requestsLinkMobile")
+  ];
+
+  placeholders.forEach(placeholder => {
+    if (placeholder) {
+      placeholder.outerHTML = linkHTML;
+    }
+  });
+});
