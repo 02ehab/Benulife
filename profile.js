73ed8c83e0f6012fbbb3d1a18a497fd7bfc2a59f
Ehab <40764172+02ehab@users.noreply.github.com>
2025-07-15 18:16:16 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -193,3 +193,20 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("userCity").textContent = userData.city || "غير محددة";
   document.querySelector(".blood-type-badge").textContent = userData.bloodType || "N/A";
 });
+//وقت تسجيل الدخول يظهر ملفي ويختفي تسجيل الدخول
+ document.addEventListener("DOMContentLoaded", function () {
+    const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+
+    const authButtons = document.getElementById("authButtons");
+    const sideAuthButtons = document.getElementById("sideAuthButtons");
+
+    const profileLink = document.getElementById("profileLink");
+    const profileLinkMobile = document.getElementById("profileLinkMobile");
+
+    if (isLoggedIn) {
+      if (authButtons) authButtons.style.display = "none";
+      if (sideAuthButtons) sideAuthButtons.style.display = "none";
+      if (profileLink) profileLink.style.display = "inline-block";
+      if (profileLinkMobile) profileLinkMobile.style.display = "inline-block";
+    }
+  });
