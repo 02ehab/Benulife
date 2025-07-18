6d6bad7033b591852c650c1dd0770d7174432120
Ehab <40764172+02ehab@users.noreply.github.com>
2025-07-18 14:09:12 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -131,7 +131,7 @@
   }
 });
 
-// تغيير صفحة الطلبات علي نوع المستخدم
+ // تغيير صفحة الطلبات علي نوع المستخدم
 document.addEventListener("DOMContentLoaded", () => {
   const userType = localStorage.getItem("userType"); // "donor", "hospital", "bloodbank"
 
@@ -158,6 +158,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
 
 // قائمة منسدلة
 document.addEventListener("DOMContentLoaded", () => {
@@ -193,20 +194,3 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("userCity").textContent = userData.city || "غير محددة";
   document.querySelector(".blood-type-badge").textContent = userData.bloodType || "N/A";
 });
-//وقت تسجيل الدخول يظهر ملفي ويختفي تسجيل الدخول
- document.addEventListener("DOMContentLoaded", function () {
-    const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
-
-    const authButtons = document.getElementById("authButtons");
-    const sideAuthButtons = document.getElementById("sideAuthButtons");
-
-    const profileLink = document.getElementById("profileLink");
-    const profileLinkMobile = document.getElementById("profileLinkMobile");
-
-    if (isLoggedIn) {
-      if (authButtons) authButtons.style.display = "none";
-      if (sideAuthButtons) sideAuthButtons.style.display = "none";
-      if (profileLink) profileLink.style.display = "inline-block";
-      if (profileLinkMobile) profileLinkMobile.style.display = "inline-block";
-    }
-  });
