37de209525e4f6ad5e44ec3bfbb6df24de3571b9
Ehab <40764172+02ehab@users.noreply.github.com>
2025-07-18 14:59:05 +0300

    Update privacy.js

--- privacy.js
+++ privacy.js
@@ -6,6 +6,23 @@ window.closeMenu = function () {
   document.getElementById("sideMenu").classList.remove("open");
 }
 
+// إظهار رابط "ملفي" واختفاء تسجيل الدخول عند تسجيل الدخول
+document.addEventListener("DOMContentLoaded", function () {
+  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+
+  const authButtons = document.getElementById("authButtons");
+  const sideAuthButtons = document.getElementById("sideAuthButtons");
+
+  const profileLink = document.getElementById("profileLink");
+  const profileLinkMobile = document.getElementById("profileLinkMobile");
+
+  if (isLoggedIn) {
+    if (authButtons) authButtons.style.display = "none";
+    if (sideAuthButtons) sideAuthButtons.style.display = "none";
+    if (profileLink) profileLink.style.display = "inline-block";
+    if (profileLinkMobile) profileLinkMobile.style.display = "inline-block";
+  }
+});
   // تغيير صفحة الطلبات علي نوع المستخدم
 document.addEventListener("DOMContentLoaded", () => {
   const userType = localStorage.getItem("userType"); // "donor", "hospital", "bloodbank"
